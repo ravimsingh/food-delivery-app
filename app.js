9133bcb2b2737ab3b425a6f7534d7b204073e623
Ravi Mohan Singh <ravimohansingh@Ravis-MacBook-Air.local>
2023-10-25 01:11:21 +0530

    lecture 03 completed

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parrent" }, [
-  React.createElement("div", { id: "children1", key: 6 }, [
-    React.createElement("h1", { key: "1" }, "I am h1 tag"),
-    React.createElement("h2", { key: "2" }, "I am h2 tag"),
-  ]),
-  ,
-  React.createElement("div", { id: "children2", key: 3 }, [
-    React.createElement("h1", { key: "1" }, "I am h1 tag"),
-    React.createElement("h2", { key: "2" }, "I am h2 tag"),
-  ]),
-]);
+const heading = <h1 className="heading">This is namaste React Course</h1>;
 
-// const heading = React.createElement("h1", {}, "I am h1 tag");
+const SecondComponent = () => <h2>this is second component</h2>;
 
-//console.log(parent);
+const FunctionalComponents = () => {
+  return (
+    <div>
+      {heading}
+      <h1>This is a functional component</h1>
+      <SecondComponent />
+      {SecondComponent()}
+      <SecondComponent></SecondComponent>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<FunctionalComponents />);
