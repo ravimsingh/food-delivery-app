9bb033b3e034972f22f3242c7a1b8bc28135d03e
Ravi Mohan Singh <ravimohansingh@Ravis-MacBook-Air.local>
2023-10-24 22:57:04 +0530

    second lecture finished

--- app.js
+++ app.js
@@ -1,30 +1,21 @@
-/**
- * <div id="parrent">
- *  <div id="children">
- *  <h1>I am h1 tag</h1>
- *  </div>
- * <div id="children2">
- *  <h1>I am h1 tag</h1>
- *  </div>
- * </div>
- *
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parrent" }, [
-  React.createElement("div", { id: "children1" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
+  React.createElement("div", { id: "children1", key: 6 }, [
+    React.createElement("h1", { key: "1" }, "I am h1 tag"),
+    React.createElement("h2", { key: "2" }, "I am h2 tag"),
   ]),
   ,
-  React.createElement("div", { id: "children2" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
+  React.createElement("div", { id: "children2", key: 3 }, [
+    React.createElement("h1", { key: "1" }, "I am h1 tag"),
+    React.createElement("h2", { key: "2" }, "I am h2 tag"),
   ]),
 ]);
 
 // const heading = React.createElement("h1", {}, "I am h1 tag");
 
-console.log(parent);
+//console.log(parent);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
